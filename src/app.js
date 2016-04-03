3bd22267e5c15e2f4e134e65e3a0fcff6b7d2580
Giovanni Lovato <heruan@aldu.net>
2016-04-03 17:12:51 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+export class App {
+  configureRouter(config, router) {
+    config.title = 'Aurelia';
+    config.map([{
+      route: ['', 'welcome'],
+      name: 'welcome',
+      moduleId: './welcome',
+      nav: true,
+      title: 'Welcome'
+    }, {
+      route: 'users',
+      name: 'users',
+      moduleId: './users',
+      nav: true,
+      title: 'Github Users'
+    }, {
+      route: 'chart',
+      name: 'chart',
+      moduleId: './chart',
+      nav: true,
+      title: 'Chart'
+    }, ]);
+
+    this.router = router;
+  }
+}
---
+++ src/chart.js
@@ -0,0 +1,28 @@
+import Chart from "chart";
+
+export class ChartModule {
+
+  attached() {
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3]
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+
+}
